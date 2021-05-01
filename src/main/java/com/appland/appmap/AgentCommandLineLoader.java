8b61fd5bb9c626d9cdb5b4c62bf6017e5baa0bf3
Marcus Sanchez <m4rkuz@gmail.com>
2021-05-01 01:15:05 -0500

    Addressed PR comments and ran checkstyle with google settings.

--- src/main/java/com/appland/appmap/AgentCommandLineLoader.java
+++ src/main/java/com/appland/appmap/AgentCommandLineLoader.java
@@ -2,6 +2,8 @@ package com.appland.appmap;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.List;
 import org.gradle.api.GradleException;
 import org.gradle.api.Named;
 import org.gradle.api.logging.Logger;
@@ -10,54 +12,62 @@ import org.gradle.api.tasks.Internal;
 import org.gradle.process.CommandLineArgumentProvider;
 import org.gradle.util.RelativePathUtil;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * This class is the actual responsible to build the JVM args to run the Appmap Agent.
+ */
 public class AgentCommandLineLoader implements CommandLineArgumentProvider, Named {
-    private static final Logger LOGGER = Logging.getLogger(CommandLineArgumentProvider.class);
 
-    private final AppmapPluginExtension appmap;
+  private static final Logger LOGGER = Logging.getLogger(CommandLineArgumentProvider.class);
 
-    public AgentCommandLineLoader(AppmapPluginExtension appmap) {
-        this.appmap = appmap;
+  private final AppmapPluginExtension appmap;
+
+  public AgentCommandLineLoader(AppmapPluginExtension appmap) {
+    this.appmap = appmap;
+  }
+
+  @Override
+  public Iterable<String> asArguments() {
+    return getAsJvmArg();
+  }
+
+  @SuppressWarnings("NullableProblems")
+  @Internal
+  @Override
+  public String getName() {
+    return "appmapAgent";
+  }
+
+  /**
+   * This method is called by the internal Compiler options to get the command needed to run at
+   * command line.
+   *
+   * @return JVM args needed to run the appmap agent.
+   */
+  @Internal
+  public List<String> getAsJvmArg() {
+    if (!appmap.isConfigFileValid()) {
+      appmap.setSkip(true);
+      throw new GradleException("Configuration file must exist and be readable: "
+          + appmap.getConfigFile().get().getAsFile().getPath());
     }
-
-    @Override
-    public Iterable<String> asArguments() {
-        return getAsJvmArg();
+    if (appmap.shouldSkip()) {
+      LOGGER.warn("Appmap task was executed but but is disable, skip property set to " + appmap
+          .shouldSkip());
+      return new ArrayList<>();
+    } else {
+      String builder = "-javaagent:"
+          + RelativePathUtil.relativePath(
+          appmap.project.getProjectDir(), appmap.getAgentConf().getSingleFile()
+      );
+      List<String> argumentLn = ImmutableList.of(
+          builder,
+          "-Dappmap.config.file=" + appmap.getConfigFile().get().toString(),
+          "-Dappmap.output.directory=" + appmap.getOutputDirectory().get().toString(),
+          "-Dappmap.event.valueSize=" + appmap.getEventValueSize(),
+          "-Dappmap.debug=" + appmap.getDebug()
+      );
+      LOGGER.lifecycle("Arguments line set to " + Joiner.on(",").join(argumentLn));
+      return argumentLn;
     }
-
-    @SuppressWarnings("NullableProblems")
-    @Internal
-    @Override
-    public String getName() {
-        return "appmapAgent";
-    }
-
-    @Internal
-    public List<String> getAsJvmArg() {
-        if (!appmap.isConfigFileValid()) {
-            appmap.setSkip(true);
-            throw new GradleException("Configuration file must exist and be readable: "
-                    + appmap.getConfigFile().get().getAsFile().getPath());
-        }
-        if (appmap.shouldSkip()) {
-            LOGGER.warn("Appmap task was executed but but is disable, skip property set to " + appmap.shouldSkip());
-            return new ArrayList<>();
-        } else {
-            String builder = "-javaagent:" +
-                    RelativePathUtil.relativePath(
-                            appmap.project.getProjectDir(), appmap.getAgentConf().getSingleFile()
-                    );
-            List<String> argumentLn = ImmutableList.of(
-                    builder,
-                    "-Dappmap.config.file=" + appmap.getConfigFile().get().toString(),
-                    "-Dappmap.output.directory=" + appmap.getOutputDirectory().get().toString(),
-                    "-Dappmap.event.valueSize=" + appmap.getEventValueSize(),
-                    "-Dappmap.debug=" + appmap.getDebug()
-            );
-            LOGGER.lifecycle("Arguments line set to " + Joiner.on(",").join(argumentLn));
-            return argumentLn;
-        }
-    }
+  }
 }
--- src/main/java/com/appland/appmap/CleanOutputDirectoryAction.java
+++ src/main/java/com/appland/appmap/CleanOutputDirectoryAction.java
@@ -1,32 +1,35 @@
 package com.appland.appmap;
 
+import java.io.File;
 import org.gradle.api.Action;
 import org.gradle.api.GradleException;
 import org.gradle.api.Task;
 import org.gradle.api.file.FileSystemOperations;
 import org.gradle.api.provider.Provider;
 
-import java.io.File;
-
+/**
+ * The action executed for cleaning the output directory before each run.
+ */
 public class CleanOutputDirectoryAction implements Action<Task> {
-    private final FileSystemOperations fs;
-    private final Provider<File> outputDirectory;
 
-    public CleanOutputDirectoryAction(FileSystemOperations fs, Provider<File>  outputDirectory) {
-        this.fs = fs;
-        this.outputDirectory = outputDirectory;
-    }
+  private final FileSystemOperations fs;
+  private final Provider<File> outputDirectory;
 
-    @Override
-    public void execute(Task task) {
-        File outputDirectoryFile = outputDirectory.get();
-        if (outputDirectoryFile == null) {
-            throw new GradleException(" destination file must not be null if output type is FILE");
-        }
-        if (fs.delete(spec -> spec.delete(outputDirectoryFile)).getDidWork()) {
-            task.getLogger().info("Appmap output directory cleared");
-        } else {
-            task.getLogger().info("Output directory already cleared or is read only.");
-        }
+  public CleanOutputDirectoryAction(FileSystemOperations fs, Provider<File> outputDirectory) {
+    this.fs = fs;
+    this.outputDirectory = outputDirectory;
+  }
+
+  @Override
+  public void execute(Task task) {
+    File outputDirectoryFile = outputDirectory.get();
+    if (outputDirectoryFile == null) {
+      throw new GradleException(" destination file must not be null if output type is FILE");
     }
+    if (fs.delete(spec -> spec.delete(outputDirectoryFile)).getDidWork()) {
+      task.getLogger().info("Appmap output directory cleared");
+    } else {
+      task.getLogger().info("Output directory already cleared or is read only.");
+    }
+  }
 }
